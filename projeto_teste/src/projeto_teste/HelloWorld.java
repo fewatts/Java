59b6d7bb6614e797953f5891d336e815a67464eb
Fewatts <feralveswatts@gmail.com>
2023-02-07 16:14:31 -0300

    Atualizando gitignore

---
+++ projeto_teste/src/projeto_teste/HelloWorld.java
@@ -0,0 +1,11 @@
+package projeto_teste;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+		System.out.println("A turma 61 é a melhor!");
+
+	}
+
+}
