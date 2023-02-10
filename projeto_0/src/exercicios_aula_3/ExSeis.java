d626f08dfa61398fa1b9c95a8a8a747dc1a0ec93
Fewatts <feralveswatts@gmail.com>
2023-02-10 16:24:55 -0300

    Ex completos

--- projeto_0/src/exercicios_aula_3/ExSeis.java
+++ projeto_0/src/exercicios_aula_3/ExSeis.java
@@ -8,18 +8,23 @@ public class ExSeis {
 
 	public static void main(String[] args) {
 		int n, soma_mult = 0, count = 0;
+		float media = 0;
 		
 		do {
-			System.out.println("Digite um número: ");
+			System.out.println("Digite um número [0 para]: ");
 			n = leia.nextInt();
 			
-			if(n % 3 == 0) {
-				soma_mult += n;
-				count += 1;
+			if(n > 0) {
+				if(n % 3 == 0) {
+					soma_mult += n;
+					count += 1;
+				}
 			}
-		}while(n != 0 && n > 0);
-		System.out.println("A média de todos os números múltiplos de 3 é: "+ soma_mult / count);
+			
+		}while(n != 0);
+		media = soma_mult / count;
+		System.out.printf("A média de todos os números múltiplos de 3 é: %.2f", media);
 
 	}
-
+		
 }
