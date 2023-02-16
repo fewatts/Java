f84f39a2a6ea0a2efaf094ecac8a9ac08b378d5a
Fewatts <feralveswatts@gmail.com>
2023-02-16 16:36:26 -0300

    Exercício aula 6

---
+++ projeto_0/src/exercicios_aula_6/Calculos.java
@@ -0,0 +1,16 @@
+package exercicios_aula_6;
+
+public class Calculos {
+	 public double soma(double a, double b) {
+		return a + b;
+	}
+	 public double subtracao(double a, double b) {
+		return a - b;
+	}
+	 public double multiplicacao(double a, double b) {
+		return a * b;
+	}
+	 public double divisao(double a, double b) {
+		return a / b;
+	}
+}
---
+++ projeto_0/src/exercicios_aula_6/MenuCalculadora.java
@@ -0,0 +1,40 @@
+package exercicios_aula_6;
+
+import java.util.Scanner;
+
+public class MenuCalculadora {
+	
+	static Scanner leia = new Scanner(System.in);
+	static Calculos calc = new Calculos();
+
+	public static void main(String[] args) {
+		double a, b;
+		int op;
+		
+		while(true) {
+			System.out.println("\n1-Soma");
+			System.out.println("2-Subtração");
+			System.out.println("3-Multiplicação");
+			System.out.println("4-Divisão");
+			System.out.println("0-Sair do programa");
+			System.out.println(">>>Sua opção: ");
+			op = leia.nextInt();
+			if(op == 0)
+				System.exit(0);
+			System.out.println("Digite o 1º valor: ");
+			a = leia.nextDouble();
+			System.out.println("Digite o 2º valor: ");
+			b = leia.nextDouble();
+			switch(op) {//Se for mais de um comando usar chaves
+			case 1 -> System.out.println("\nSoma: " + calc.soma(a, b));
+			case 2 -> System.out.println("Subtração: "+ calc.subtracao(a, b));
+			case 3 -> System.out.println("Multiplicação: "+ calc.multiplicacao(a, b));
+			case 4 -> System.out.println("Divisão: " + calc.divisao(a, b));
+			default -> System.out.println("Opção inválida...");
+			}
+			
+		}
+		
+	}
+	
+}
