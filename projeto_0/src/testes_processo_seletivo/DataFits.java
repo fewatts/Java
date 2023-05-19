e04677775c11c1ac9bb811532d7327b8d28e8a8f
Fewatts <feralveswatts@gmail.com>
2023-05-19 17:10:40 -0300

    EX
    HackerRank

---
+++ projeto_0/src/testes_processo_seletivo/DataFits.java
@@ -0,0 +1,38 @@
+package testes_processo_seletivo;
+
+import java.util.Scanner;
+
+public class DataFits {
+    static String fitDataType(String numString) {
+        String answer = "";
+        try {
+            long num = Long.parseLong(numString);
+            answer = numString + " can be fitted in:\n";
+            if ((num <= Byte.MAX_VALUE) && (num >= Byte.MIN_VALUE)) {
+                answer = answer.concat("¬¬ byte\n¬¬ short\n¬¬ int\n¬¬ long");
+            } else if ((num <= Short.MAX_VALUE) && (num >= Short.MIN_VALUE)) {
+                answer = answer.concat("¬¬ short\n¬¬ int\n¬¬ long");
+            } else if ((num <= Integer.MAX_VALUE) && (num >= Integer.MIN_VALUE)) {
+                answer = answer.concat("¬¬ int\n¬¬ long");
+            } else {
+                answer = answer.concat("¬¬ long");
+            }
+        } catch (NumberFormatException e) {
+            answer = numString + " can't be fitted anywhere (primitive Types).";
+        }
+        return answer;
+    }
+
+    static Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) {
+        System.out.println("Number of tests:");
+        int testTimes = sc.nextInt();
+        for(int i = 0; i < testTimes; i++){
+            System.out.println("Enter the number for test:");
+            String stringNum = sc.next();
+            System.out.println(fitDataType(stringNum));
+        }
+
+    }
+
+}
---
+++ projeto_0/src/testes_processo_seletivo/TestBH.java
@@ -0,0 +1,19 @@
+package testes_processo_seletivo;
+
+import java.util.Scanner;
+
+public class TestBH {
+    static Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) {
+        int B = 0, H = 0;
+        B = sc.nextInt();
+        H = sc.nextInt();   
+        if (B <= 0 || H <= 0) {
+            System.out.println("java.lang.Exception: Breadth and height must be positive");
+        }else{
+            System.out.println(B * H);
+        } 
+
+    }
+    
+}
