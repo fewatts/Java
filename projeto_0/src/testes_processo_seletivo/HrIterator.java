1f12f2b55da4442c13fa6e8f4ef3bdf9b4d54d5b
Fewatts <feralveswatts@gmail.com>
2023-06-07 17:30:30 -0300

    EX
    Iterator filter

---
+++ projeto_0/src/testes_processo_seletivo/HrIterator.java
@@ -0,0 +1,50 @@
+package testes_processo_seletivo;
+
+// import java.util.ArrayList;
+// import java.util.Iterator;
+// import java.util.Scanner;
+
+public class HrIterator {
+
+    // static Scanner in = new Scanner(System.in);
+
+    // static Iterator func(ArrayList mylist) {
+
+    //     Iterator it = mylist.iterator();
+        
+    //     while (it.hasNext()) {
+    //         Object element = it.next();
+    //         if (element instanceof String) 
+    //             break;
+    //     }
+    //     return it;
+    // }
+
+    // public static void main(String[] argh) {
+
+    //     ArrayList mylist = new ArrayList();
+        
+    //     int n = in.nextInt();
+        
+    //     int m = in.nextInt();
+    //     for (int i = 0; i < n; i++) {
+    //         mylist.add(in.nextInt());
+        
+    //     }
+        
+    //     mylist.add("###");
+        
+    //     for (int i = 0; i < m; i++) {
+    //         mylist.add(in.next());
+    //     }
+
+    //     Iterator it = func(mylist);
+        
+    //     while (it.hasNext()) {
+    //         Object element = it.next();
+    //         System.out.println((String) element);
+    //     }
+
+    // }
+
+}
