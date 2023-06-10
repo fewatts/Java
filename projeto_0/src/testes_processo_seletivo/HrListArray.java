961a8c04b9c55fa1b8425f6080ddec31fbfc1e9e
Fewatts <feralveswatts@gmail.com>
2023-06-09 21:05:04 -0300

    Solving the challange for IBM

--- projeto_0/src/testes_processo_seletivo/HrListArray.java
+++ projeto_0/src/testes_processo_seletivo/HrListArray.java
@@ -4,18 +4,27 @@ import java.util.ArrayList;
 
 public class HrListArray {
 
-    public static int checkprice(ArrayList<String> produtos, ArrayList<Float> precoProdutos, ArrayList<String> produtosVendidos, ArrayList<Float> vendaProdutos){
+    public static int checkprice(
+        ArrayList<String> produtos, 
+        ArrayList<Float> precoProdutos, 
+        ArrayList<String> produtosVendidos, 
+        ArrayList<Float> produtosVendidosPreco
+        ){
         int error = 0;
-        for(int j = 0; j < produtosVendidos.size(); j++){
-            String nomeProd = produtos.get(j);
-            Float precoProd = precoProdutos.get(j);
-            String vendaProd = produtosVendidos.get(j);
-            Float precoVenda = vendaProdutos.get(j);
-            if(produtos.indexOf(nomeProd) == precoProdutos.indexOf(precoProd) && nomeProd.equalsIgnoreCase(vendaProd) && !precoProd.equals(precoVenda)){
-                error++;
+        for (int i = 0; i < produtosVendidos.size(); i++){
+            String produtoVendido = produtosVendidos.get(i);
+            Float produtoVendidoPreco = produtosVendidosPreco.get(i);
+
+            int produtoIndex = produtos.indexOf(produtoVendido);
+
+            if(produtoIndex >= 0 && produtoIndex < produtosVendidos.size() && i < produtosVendidosPreco.size()){
+                if(!precoProdutos.get(produtoIndex).equals(produtoVendidoPreco)){
+                    error++;
+                }
+
             }
 
-        }
+        } 
         return error;
     }
 
@@ -37,9 +46,11 @@ public class HrListArray {
         produtosVendidos.add("egg");
         produtosVendidos.add("cheese");
         produtosVendidos.add("milk");
+        produtosVendidos.add("milk");
         vendaProdutos.add(4.55f);//errado
         vendaProdutos.add(2.55f);//errado
         vendaProdutos.add(5.54f);//cert0
+        vendaProdutos.add(5.55f);//errado
 
         resultado = checkprice(produtos, precoProdutos, produtosVendidos, vendaProdutos);
         System.out.println("Total errors: " + resultado);
